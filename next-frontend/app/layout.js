d20df309515face445ff346c9d65e600b5629eb0
Sultan Al Muhon <sultan.al.muhon@gmail.com>
2024-06-24 16:09:19 +0600

    Added sidebar menu and basic route pages

--- next-frontend/app/layout.js
+++ next-frontend/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Sidebar from "@/app/components/Sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Sidebar>{children}</Sidebar>
+      </body>
     </html>
   );
 }
